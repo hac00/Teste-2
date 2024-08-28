6a9ddae8e0e45c59f4130885094e0e42ce006d43
hac00 <henriqueac00@gmail.com>
2024-08-27 23:07:01 -0300

    testandogit1.py

--- testandogit1.py
+++ testandogit1.py
@@ -1,3 +1,3 @@
-a = input("Digite um numero: ")
-b = input("Digite outro numero: ")
+a = int(input("Digite um numero: "))
+b = int(input("Digite outro numero: "))
 print(f"O resultado da soma dos numeros eh: {a+b}")
